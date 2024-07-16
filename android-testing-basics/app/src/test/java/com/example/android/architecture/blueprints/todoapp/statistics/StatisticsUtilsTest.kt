799dcffa38b008b74de20b8c6f60b3889c380f96
heshan_sandeepa <heshan.sandeepa@freshmilksoftware.com>
2024-07-16 14:27:00 +0530

    first commit

---
+++ android-testing-basics/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,66 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        // Create an active task (the false makes this active)
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = false)
+        )
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_oneCompleted_noActive_returnsZeroHundred() {
+        // Create an active task (the false makes this active)
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = true),
+            Task("title2", "desc2", isCompleted = true)
+        )
+        // Call your function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+        // When there are no tasks
+        val result = getActiveAndCompletedStats(emptyList())
+
+        // Both active and completed tasks are 0
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_both_returnsFortySixty() {
+        // Given 3 completed tasks and 2 active tasks
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false)
+        )
+        // When the list of tasks is computed
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Then the result is 40-60
+        assertThat(result.activeTasksPercent, `is`(40f))
+        assertThat(result.completedTasksPercent, `is`(60f))
+    }
+}
---
+++ android-testing-basics/app/src/test/java/com/example/android/architecture/blueprints/todoapp/test/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.example.android.architecture.blueprints.todoapp.test
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    // Each test is annotated with @Test (this is a Junit annotation)
+    @Test
+    fun addition_isCorrect() {
+        // Here you are checking that 4 is the same as 2+2
+        assertEquals(4, 2 + 2)
+    }
+}
